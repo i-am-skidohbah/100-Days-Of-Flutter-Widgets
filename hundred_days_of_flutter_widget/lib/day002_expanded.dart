48e10ca05364c06e60770ac52d2ac6aaa15339dc
i-am-skidohbah <109022856+i-am-skidohbah@users.noreply.github.com>
2022-10-15 15:45:18 +0100

    Update day002_expanded.dart
    added "?" 
    🙂

--- hundred_days_of_flutter_widget/lib/day002_expanded.dart
+++ hundred_days_of_flutter_widget/lib/day002_expanded.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class Day2Expanded extends StatelessWidget {
-  const Day2Expanded({Key key}) : super(key: key);
+  const Day2Expanded({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
